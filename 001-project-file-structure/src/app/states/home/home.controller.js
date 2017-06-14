c80acdc3663d45adea41505f4faa455dc4d7285f
Ricardo Valencia <ricardovalquin@gmail.com>
2017-06-14 08:12:08 -0500

    weather finished

--- 001-project-file-structure/src/app/states/home/home.controller.js
+++ 001-project-file-structure/src/app/states/home/home.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   angular
@@ -16,6 +16,7 @@
 
     vm.mainTitle = SITE_NAME;
     vm.user = loggedUser;
+    vm.weatherVariable = 0;
     vm.options = {
       center: {
         lat: 4.740675384778373,
@@ -33,44 +34,57 @@
       }
     };
 
-    var clickOnMap = $scope.$on('leafletDirectiveMap.map.click', function(event, args) {
+    function showData(info) {
+      alert = $mdDialog.alert({
+        title: info.variable,
+        textContent: info.description,
+        ok: 'Close'
+      });
+
+      $mdDialog
+        .show(alert)
+        .finally(function () {
+          alert = undefined;
+        });
+    }
+
+    var clickOnMap = $scope.$on('leafletDirectiveMap.map.click', function (event, args) {
       latlng = args.leafletEvent.latlng;
-      console.log(latlng);
-      vm.options.markers = {medellin:{lat: latlng.lat, lng: latlng.lng}};
+      vm.weatherVariable = parseInt(vm.weatherVariable);
+      vm.options.markers = {medellin: {lat: latlng.lat, lng: latlng.lng}};
       coordinates.lat = latlng.lat;
       coordinates.lon = latlng.lng;
 
-      WeatherService.getWeather(coordinates).then(function (response){
-        return response;
-
-      });
+      switch (vm.weatherVariable) {
+        case 0:
+          WeatherService.getWeather(coordinates).then(function (response) {
+            var info = {
+              variable: 'weather',
+              description: 'This is the weather for: ' + response.name + ' and today we are going to have: ' + response.weather[0].description
+            };
+            showData(info);
+          });
+          break;
+        case 1:
+          coordinates.lat = parseInt(latlng.lat) + 0.25;
+          coordinates.lon = parseInt(latlng.lng) + 0.25;
+          var radiationCoordinates = {
+            location: coordinates.lat + ',' + coordinates.lon
+          };
+          WeatherService.getRadiation(radiationCoordinates).then(function (response) {
+            var info = {
+              variable: 'Radiation',
+              description: 'This is the radiation level for the coordinates: ' + latlng.lat + ', ' + latlng.lng + ' and the data is: ' + response.data
+            };
+            showData(info);
+          });
+          break;
+      }
     });
 
 
-
-
-    var pollutionLatLon = {
-      lat: 39.25,
-      lon: 0.75
-    };
-
-    var radiationCoordinates = {
-      location: coordinates.lat + ',' + coordinates.lon
-    };
-
-    var pollutionCoordinates = {
-      location: pollutionLatLon.lat + ',' + pollutionLatLon.lon
-    };
-
-
-
-    //WeatherService.getRadiation(radiationCoordinates).then(function (response){
-    //  return response;
-    //});
-
-    //WeatherService.getPollution(pollutionCoordinates).then(function (response){
-    //  console.log(response);
-    //  return response;
-    //});
+    $scope.$on('$destroy', function () {
+      clickOnMap();
+    });
   }
 })();
